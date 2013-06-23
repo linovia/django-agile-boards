8d5e968f8cebe24758fdda5f67eadb50e692cf6b
Xavier Ordoquy <xordoquy@linovia.com>
2013-06-23 11:40:06 +0200

    First draft with Kanban board.

---
+++ agileboards/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'agileboards.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', TemplateView.as_view(template_name="board.html"), name="homepage"),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
