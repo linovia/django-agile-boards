07137911d0f602b8086a2630eb1fdf717445766c
Xavier Ordoquy <xordoquy@linovia.com>
2013-11-18 23:27:18 +0100

    Added the PUT request.

--- agileboards/kanban/static/js/angular-django-rest-resource.js
+++ agileboards/kanban/static/js/angular-django-rest-resource.js
@@ -147,6 +147,7 @@ angular.module('djangoRESTResources', ['ng']).
     var DEFAULT_ACTIONS = {
       'get':    {method:'GET'},
       'save':   {method:'POST'},
+      'update': {method: 'PUT'},
       'query':  {method:'GET', isArray:true},
       'remove': {method:'DELETE'},
       'delete': {method:'DELETE'}
