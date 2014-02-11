22aefe64759613ec6ca8b29f6701aa4dd8a2c894
Xavier Ordoquy <xordoquy@linovia.com>
2014-02-11 23:20:08 +0100

    Bumped the version.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='django-agile-boards',
-    version='0.1.0',
+    version='0.1.1',
     author='Xavier Ordoquy',
     author_email='xordoquy@linovia.com',
     url='https://www.linovia.com',
